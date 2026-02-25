22679065c97aaefd17600ff5961974de1dc9f555
Railgun <64968531+Railgun-wiki@users.noreply.github.com>
2026-02-26 01:57:26 +0800

    Update diy-before-feeds.sh to remove unused commands
    Comment out unnecessary git clone commands and patch for Python compatibility.

--- diy/ramips_mt7621/newifi_d2/diy-before-feeds.sh
+++ diy/ramips_mt7621/newifi_d2/diy-before-feeds.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 
-# Use master branch of helloworld
-git clone --depth=1 https://github.com/fw876/helloworld package/helloworld
+# 修改：移除不需要的科学上网面板源码（我们将使用 ShellCrash）
+# git clone --depth=1 https://github.com/fw876/helloworld package/helloworld
+# git clone --depth=1 https://github.com/xiaorouji/openwrt-passwall package/passwall
 
-# Use master branch of passwall
-git clone --depth=1 https://github.com/xiaorouji/openwrt-passwall package/passwall
+# 修改：添加你指定的 Minieap 校园网核心与配套 LuCI 面板
+git clone --depth=1 https://github.com/undefined443/openwrt-minieap-sysu package/minieap
+git clone --depth=1 https://github.com/kongfl888/luci-app-minieap package/luci-app-minieap
 
-# Patch include/prereq-build.mk for building on distros with newer python versions
-patch -p1 < ~/work/Railgun/Railgun/patches/00_python3.patch
+# 修改：注释掉原作者修复 Python 的补丁文件（Ubuntu 22.04 原生完美支持编译 5.10 内核，无需此补丁，保留此行反而会因为找不到补丁文件而报错退出）
+# patch -p1 < ~/work/Railgun/Railgun/patches/00_python3.patch
